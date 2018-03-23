d2761835ab2d3038f1ea32f5aa4afcf11b17eb19
pashtunzoy <asifkhannoorzai@gmail.com>
2018-03-23 23:41:42 +1100

    Removed the other theme as it isn't needed

--- prologue/assets/js/main.js
+++ prologue/assets/js/main.js
@@ -117,7 +117,7 @@
 	});
 
 	if ("serviceWorker" in navigator) {
-		navigator.serviceWorker.register("/serviceworker.js").then(function (registration) {
+		navigator.serviceWorker.register("assets/serviceworker.js").then(function (registration) {
 			console.log("Service Worker registered with scope:", registration.scope);
 		}).catch(function (err) {
 			console.log("Service Worker registration failed:", err);
---
+++ prologue/assets/serviceworker.js
@@ -0,0 +1,32 @@
+var CACHE_NAME = "prologue-cache-v1";
+var DATA_CACHE_NAME = 'prologue-v1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/images/avatar.jpg'
+];
+
+self.addEventListener('install', function (e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
