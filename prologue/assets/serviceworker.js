89d49e0f01b5f421b82505dfe3a39567a350921d
pashtunzoy <asifkhannoorzai@gmail.com>
2018-03-24 23:49:04 +1100

    Removed some unwanted characters

--- prologue/assets/serviceworker.js
+++ prologue/assets/serviceworker.js
@@ -5,22 +5,22 @@ var filesToCache = [
 ];
 
 self.addEventListener('install', function (e) {
-  console.log('[ServiceWorker] Install');
+  console.log('ServiceWorker Install');
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log('[ServiceWorker] Caching app shell');
+      console.log('ServiceWorker Caching app shell');
       return cache.addAll(filesToCache);
     })
   );
 });
 
 self.addEventListener('activate', function (e) {
-  console.log('[ServiceWorker] Activate');
+  console.log('ServiceWorker Activate');
   e.waitUntil(
     caches.keys().then(function (keyList) {
       return Promise.all(keyList.map(function (key) {
         if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
+          console.log('ServiceWorker Removing old cache', key);
           return caches.delete(key);
         }
       }));
