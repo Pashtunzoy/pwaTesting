a2c42709796de31527550802617dea7be1679212
pashtunzoy <asifkhannoorzai@gmail.com>
2018-03-25 23:20:36 +1100

    Added fetch functionality to the service worker

--- prologue/assets/serviceworker.js
+++ prologue/assets/serviceworker.js
@@ -30,3 +30,22 @@ self.addEventListener('activate', function (e) {
   );
   return self.clients.claim();
 });
+
+self.addEventListener('fetch', event => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return caches.open(RUNTIME).then(cache => {
+          return fetch(event.request).then(response => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
